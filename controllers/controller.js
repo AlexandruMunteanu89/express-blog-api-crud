b16a6a7b921ca634d78321cd09a875d357ad9c8d
AlexandruMunteanu89 <sasamunteanu18@gmail.com>
2026-06-07 18:41:13 +0200

    Implementato le logiche  CRUD

--- controllers/controller.js
+++ controllers/controller.js
@@ -4,8 +4,60 @@ const posts = require("../data/posts");
 // elenco funzioni relative alle rotte della risorsa post
 
 const index = (req, res) => {
-    res.json({ message: 'index route'});
-    console.log("rotta index");
+    const tag = req.query.tag;
+
+    if (tag) {
+        //
+        const filteredPosts = posts.filter(post => post.tag.includes(tag));
+        return res.json(filteredPosts);
+    }
+    res.json(posts);
+}
+
+
+const show = (req, res) => {
+    console.log(req.params);
+    const postId = parseInt(req.params.id);
+    console.log(postId);
+
+    // cerchiamo il post tramite id
+    const singlePost = posts.find(post => post.id === postId);
+    //imposto lo status 404
+    if (!singlePost) {
+        return res.status(404).json({ error: true, message: '404 Post not found' });
+    }
+
+
+    res.json(singlePost);
     
 }
-module.exports = {index};
+
+const destroy = (req, res) => {
+    console.log(req.params);
+    const postId = parseInt(req.params.id);
+    console.log(postId);
+    // cerchiamo il post tramite id
+    const thisPost = posts.find(post => post.id === postId);
+
+    //imposto lo status 404
+    if (!thisPost) {
+        return res.status(404).json({ error: true, message: '404 Post not found'});
+    }
+
+    //restituisco un JSON con il post trovato
+    const index = posts.indexOf(thisPost);
+
+    //Rimuoviamo il post
+    posts.splice(index, 1);
+
+    //Returniamo lo status corretto
+    res.sendStatus(204);
+
+
+
+    res.json(thisPost);
+    
+    
+}
+
+module.exports = {index, show, destroy};
--- routers/routerPosts.js
+++ routers/routerPosts.js
@@ -4,4 +4,8 @@ const router = express.Router();
 const controller = require('../controllers/controller');
 
 router.get('/', controller.index);
+// troviamo la rotta e ritorniamo una singola post
+router.get('/:id', controller.show);
+// Destroy route per cancelare un post in base al id
+router.delete('/:id', controller.destroy);
 module.exports = router;
